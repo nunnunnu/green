d73473c81fc8240644175eb09903c1a522a3397b
jjjh <110175918+nunnunnu@users.noreply.github.com>
2022-11-01 21:08:24 +0900

    N

--- green/JAVA_221101/StudentScoreMain.java
+++ green/JAVA_221101/StudentScoreMain.java
@@ -28,33 +28,43 @@ public class StudentScoreMain {
         }
       }
       else if(sel ==2 ){
-        System.out.println("===============목록================");
-        for(int i=0;i<studentlist.size();i++){
-          System.out.println(i+"번째 학생 -"+studentlist.get(i));
+        if(studentlist.size()!=0){
+          System.out.println("===============목록================");
+          for(int i=0;i<studentlist.size();i++){
+            System.out.println(i+"번째 학생 -"+studentlist.get(i));
+          }
+          System.out.print("수정할 학생의 번호를 입력하세요 : >>");  
+            int index = s.nextInt();
+            StudentScore st = studentlist.get(index);
+            String name = s.nextLine();
+            if(st.checkName(name)){
+              System.out.print("이름 : ");
+              name = s.nextLine();
+              System.out.print("점수 : ");
+              Integer score = s.nextInt();
+              studentlist.set(index, new StudentScore(name, score));
+            }
+          }
+          else{
+            System.out.println("등록된 학생이 없습니다.");
+          }
         }
-        System.out.print("수정할 학생의 번호를 입력하세요 : >>");  
-        int index = s.nextInt();
-        StudentScore st = studentlist.get(index);
-        String name = s.nextLine();
-        if(st.checkName(name)){
-          System.out.print("이름 : ");
-          name = s.nextLine();
-          System.out.print("점수 : ");
-          Integer score = s.nextInt();
-          studentlist.set(index, new StudentScore(name, score));
+        else if(sel ==3 ){
+          if(studentlist.size()!=0){
+          System.out.println("================목록==================");
+          for(int i=0;i<studentlist.size();i++){
+            System.out.println(i+"번째 학생 -"+studentlist.get(i));
+          }
+          System.out.print("삭제할 학생의 번호를 입력하세요 : >>");  
+          int index = s.nextInt();
+          studentlist.remove(index);
+          System.out.println("삭제되었습니다.");  
+        }
+        else{
+          System.out.println("등록된 학생이 없습니다.");
         }
       }
-      else if(sel ==3 ){
-        System.out.println("================목록==================");
-        for(int i=0;i<studentlist.size();i++){
-          System.out.println(i+"번째 학생 -"+studentlist.get(i));
-        }
-        System.out.print("삭제할 학생의 번호를 입력하세요 : >>");  
-        int index = s.nextInt();
-        studentlist.remove(index);
-        System.out.println("삭제되었습니다.");  
-      }
-      else if(sel ==4 ){
+        else if(sel ==4 ){
         int sum=0;
         for(int i=0;i<studentlist.size();i++){
           // StudentScore st = studentlist.get(i);
@@ -65,10 +75,12 @@ public class StudentScoreMain {
         }
         if(studentlist.size()!=0){
           System.out.println("전체 학생의 평균 : "+sum/studentlist.size());
-
+        }else{
+          System.out.println("등록된 학생이 없습니다.");
         }
+      }else{
+        System.out.println("번호를 잘못입력하셨습니다. 0~4사이의 번호를 입력해주세요");
       }
     }
-    
   }
 }
